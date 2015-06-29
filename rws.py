215d73d7b4fa98eb09e05e25014cfadd4a414329
Tom Ritchford <tom@swirly.com>
2015-06-29 18:22:22 -0400

    Input parser.

--- rws.py
+++ rws.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+# Requires https://pypi.python.org/pypi/websockets
+
 import asyncio
+import json
 import websockets
 
 from rws_args import parse_args
@@ -9,11 +12,33 @@ ARGS = parse_args('rws: ripple websocket test.')
 PROTOCOL = 'wss' if ARGS.secure else 'ws'
 ADDRESS = '%s://%s:%d/' % (PROTOCOL, ARGS.address, ARGS.port)
 
+COMMANDS = {
+    'quit': lambda: True,
+}
+
+KEYS = sorted(COMMANDS.keys())
+PROMPT = 'Choose command from ' + ', '.join(KEYS)
+
+def execute_one_input(prompt=''):
+    prompt = prompt or PROMPT
+
+    while True:
+        data = input(prompt + ': ').strip().split()
+        if data:
+            try:
+                command = COMMANDS.get(data[0])
+                if not command:
+                    raise ValueError('Don\'t understand command ' + data[0])
+                return command(*data[1:])
+            except Exception as e:
+                print('ERROR:', e)
+
+
 def client():
     @asyncio.coroutine
     def coroutine():
         websocket = yield from websockets.connect(ADDRESS)
-        name = input("What's your name? ")
+        name = input('')
         yield from websocket.send(name)
         print("> {}".format(name))
         greeting = yield from websocket.recv()
